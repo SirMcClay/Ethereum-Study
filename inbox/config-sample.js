ce667f1df379050d9052a7240705263d25060d85
sirmcclay <mcclay.design@gmail.com>
2021-10-07 12:26:32 -0300

    deployment code

---
+++ inbox/config-sample.js
@@ -0,0 +1,6 @@
+const config = {
+	mnemonic: 'PHRASE GOES HERE',
+	endpoint: 'ENDPOINT-ADDRESS-GOES-HERE',
+};
+
+module.exports = config;
--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,20 @@
-// deploy code will go here
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+const config = require('./config');
+
+const provider = new HDWalletProvider(config.mnemonic, config.endpoint);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	console.log('Attempting to deploy from account', accounts[0]);
+
+	const result = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({ data: bytecode, arguments: ['Hi there!'] })
+		.send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+
+	console.log('Contract deployed to', result.options.address);
+};
+deploy();
