9b4b61dae11998f7dbfa4d5a4cc066ddc3015ee1
sirmcclay <mcclay.design@gmail.com>
2021-10-19 04:19:08 -0300

    some fixes

---
+++ lottery-react1/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import web3 from './web3';
+
+class App extends React.Component {
+	render() {
+		console.log(web3.version);
+		web3.eth.getAccounts().then(console.log);
+
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
+}
+export default App;
