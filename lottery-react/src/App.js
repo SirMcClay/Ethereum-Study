fdab726c0551e18d5e77cad11dba4cf881596d95
sirmcclay <mcclay.design@gmail.com>
2021-10-22 10:06:45 -0300

    app finished

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,17 +1,51 @@
 import './App.css';
 import React from 'react';
+import web3 from './web3';
 import lottery from './lottery';
 
 class App extends React.Component {
 	state = {
 		manager: '',
+		players: [],
+		balance: '',
+		value: '',
+		message: '',
 	};
 
 	async componentDidMount() {
 		const manager = await lottery.methods.manager().call();
+		const players = await lottery.methods.getPlayers().call();
+		const balance = await web3.eth.getBalance(lottery.options.address);
 
-		this.setState({ manager });
+		this.setState({ manager, players, balance });
 	}
+
+	onSubmit = async (event) => {
+		event.preventDefault();
+
+		const accounts = await web3.eth.getAccounts();
+
+		this.setState({ message: 'Waiting on transaction success...' });
+
+		await lottery.methods.enter().send({
+			from: accounts[0],
+			value: web3.utils.toWei(this.state.value, 'ether'),
+		});
+
+		this.setState({ message: 'You have been entered!' });
+	};
+
+	onClick = async () => {
+		const accounts = await web3.eth.getAccounts();
+
+		this.setState({ message: 'Waiting on transaction success...' });
+
+		await lottery.methods.pickWinner().send({
+			from: accounts[0],
+		});
+
+		this.setState({ message: 'A winner has been picked!' });
+	};
 
 	render() {
 		return (
@@ -19,7 +53,32 @@ class App extends React.Component {
 				<h2>Lottery Contract</h2>
 				<p>
 					This contract is managed by <b>{this.state.manager}</b>
+					<br />
+					There are currently <b>{this.state.players.length} people</b> entered,
+					competing to win{' '}
+					<b>{web3.utils.fromWei(this.state.balance, 'ether')} ether!</b>
 				</p>
+				<hr />
+				<form onSubmit={this.onSubmit}>
+					<h4>Want to try your luck?</h4>
+					<div>
+						<label>Amount of ether to enter</label>
+						<input
+							value={this.state.value}
+							onChange={(event) => this.setState({ value: event.target.value })}
+						/>
+					</div>
+					<button>Enter</button>
+				</form>
+
+				<hr />
+
+				<h4>Ready to pick a winner?</h4>
+				<button onClick={this.onClick}>Pick a winner!</button>
+
+				<hr />
+
+				<h1>{this.state.message}</h1>
 			</div>
 		);
 	}
