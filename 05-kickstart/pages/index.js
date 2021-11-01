3889bb6961d8857386bffe90dc296d4721600269
sirmcclay <mcclay.design@gmail.com>
2021-11-01 03:16:10 -0300

    render card campaigns with semantic ui lib

--- 05-kickstart/pages/index.js
+++ 05-kickstart/pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
@@ -8,8 +9,30 @@ class CampaignIndex extends Component {
 		return { campaigns };
 	}
 
+	renderCampaigns() {
+		const items = this.props.campaigns.map((address) => {
+			return {
+				header: address,
+				description: <a>View Campaign</a>,
+				fluid: true,
+			};
+		});
+
+		return <Card.Group items={items} />;
+	}
+
 	render() {
-		return <div>{this.props.campaigns}</div>;
+		return (
+			<div>
+				<link
+					async
+					rel="stylesheet"
+					href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+				/>
+				<script src="https://cdn.jsdelivr.net/npm/semantic-ui-react/dist/umd/semantic-ui-react.min.js"></script>
+				{this.renderCampaigns()}
+			</div>
+		);
 	}
 }
 
