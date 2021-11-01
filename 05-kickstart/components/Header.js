e964e7d344fb5c28814e04465a1cc8d39f404712
sirmcclay <mcclay.design@gmail.com>
2021-11-01 10:14:57 -0300

    container, margin, tweaking a little to get things beauty

--- 05-kickstart/components/Header.js
+++ 05-kickstart/components/Header.js
@@ -3,7 +3,7 @@ import { Menu } from 'semantic-ui-react';
 
 const Header = () => {
 	return (
-		<Menu>
+		<Menu style={{ marginTop: '10px' }}>
 			<Menu.Item>CrowdCoin</Menu.Item>
 
 			<Menu.Menu position="right">
--- 05-kickstart/components/Layout.js
+++ 05-kickstart/components/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 import Header from './Header';
 
 const Layout = (props) => {
 	return (
-		<div>
+		<Container>
 			<Header />
 			{props.children}
-		</div>
+		</Container>
 	);
 };
 
