0f10c63c31a6edb7358058288689b2060ca49691
sirmcclay <mcclay.design@gmail.com>
2021-11-02 03:44:07 -0300

    input and form event handlers

--- 05-kickstart/pages/campaigns/new.js
+++ 05-kickstart/pages/campaigns/new.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import { Form, Button, Input } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 class CampaignNew extends Component {
+	state = {
+		minimumContribution: '',
+	};
+
+	onSubmit = async (event) => {
+		event.preventDefault();
+
+		const accounts = await web3.eth.getAccounts();
+		await factory.methods.crateCampaign(this.state.minimumContribution).send({
+			from: accounts[0],
+		});
+	};
+
 	render() {
 		return (
 			<Layout>
 				<h3>Create a Campaign</h3>
 
-				<Form>
+				<Form onSubmit={this.onSubmit}>
 					<Form.Field>
 						<label>Minimum Contribution</label>
-						<Input label="wei" labelPosition="right" placeholder="e.g.: 1000" />
+						<Input
+							label="wei"
+							labelPosition="right"
+							placeholder="e.g.: 1000"
+							value={this.state.minimumContribution}
+							onChange={(event) =>
+								this.setState({ minimumContribution: event.target.value })
+							}
+						/>
 					</Form.Field>
 
 					<Button primary>Create!</Button>
