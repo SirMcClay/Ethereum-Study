670e1391704a0c50d6e78e073a8b3f5694efc616
sirmcclay <mcclay.design@gmail.com>
2021-11-04 04:12:09 -0300

    start configure handlers

--- 05-kickstart/components/ContributeForm.js
+++ 05-kickstart/components/ContributeForm.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import { Form, Input, Message, Button } from 'semantic-ui-react';
 
 class ContributeForm extends Component {
+  state = {
+    value: ''
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <Form>
+      <Form onSubmit={this.onSubmit}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
+            value={this.state.value}
+            onChange={event => this.setState({value: event.target.value})}
             label="ether"
             labelPosition="right"
           />
