7761797d821700299de4327ce9689950a4d925b1
sirmcclay <mcclay.design@gmail.com>
2021-11-05 04:48:16 -0300

    little tweak of rows and columns of show grid

--- 05-kickstart/pages/campaigns/show.js
+++ 05-kickstart/pages/campaigns/show.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Grid, GridColumn, Button } from 'semantic-ui-react';
+import { Card, Grid, Button } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 import web3 from '../../ethereum/web3';
@@ -73,17 +73,21 @@ class CampaignShow extends Component {
 			<Layout>
 				<h3>Campaign Show</h3>
 				<Grid>
-					<GridColumn width={10}>
-						{this.renderCards()}
-						<Link route={`/campaigns/${this.props.address}/requests`}>
-							<a>
-								<Button primary>View Requests</Button>
-							</a>
-						</Link>
-					</GridColumn>
-					<GridColumn width={6}>
-            <ContributeForm address={this.props.address} />
-					</GridColumn>
+					<Grid.Row>
+						<Grid.Column width={10}>{this.renderCards()}</Grid.Column>
+						<Grid.Column width={6}>
+							<ContributeForm address={this.props.address} />
+						</Grid.Column>
+					</Grid.Row>
+					<Grid.Row>
+						<Grid.Column>
+							<Link route={`/campaigns/${this.props.address}/requests`}>
+								<a>
+									<Button primary>View Requests</Button>
+								</a>
+							</Link>
+						</Grid.Column>
+					</Grid.Row>
 				</Grid>
 			</Layout>
 		);
