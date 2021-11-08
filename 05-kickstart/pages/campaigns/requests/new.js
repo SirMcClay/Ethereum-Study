68b3a7ae8c167ffcabd6ff22fc3ce4a47e855b26
sirmcclay <mcclay.design@gmail.com>
2021-11-08 04:12:59 -0300

    navigation, loading and error message logic

--- 05-kickstart/pages/campaigns/requests/new.js
+++ 05-kickstart/pages/campaigns/requests/new.js
@@ -10,6 +10,8 @@ class RequestNew extends Component {
 		value: '',
 		description: '',
 		recipient: '',
+		loading: false,
+		errorMessage: '',
 	};
 
 	static async getInitialProps(props) {
@@ -24,19 +26,32 @@ class RequestNew extends Component {
 		const campaign = Campaign(this.props.address);
 		const { description, value, recipient } = this.state;
 
+		this.setState({ loading: true, errorMessage: '' });
+
 		try {
 			const accounts = await web3.eth.getAccounts();
 			await campaign.methods
 				.createRequest(description, web3.utils.toWei(value, 'ether'), recipient)
 				.send({ from: accounts[0] });
-		} catch (err) {}
+
+			Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+		} catch (err) {
+			this.setState({ errorMessage: err.message });
+		}
+
+		this.setState({ loading: false });
 	};
 
 	render() {
 		return (
 			<Layout>
+				<Layout>
+					<Link route={`/campaigns/${this.props.address}/requests`}>
+						<a>Back</a>
+					</Link>
+				</Layout>
 				<h3>Create a Request</h3>
-				<Form onSubmit={this.onSubmit}>
+				<Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
 					<Form.Field>
 						<label>Description</label>
 						<Input
@@ -65,7 +80,10 @@ class RequestNew extends Component {
 						/>
 					</Form.Field>
 
-					<Button primary>Create</Button>
+					<Message error header='Oops!' content={this.state.errorMessage} />
+					<Button primary loading={this.state.loading}>
+						Create
+					</Button>
 				</Form>
 			</Layout>
 		);
