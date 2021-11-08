dd6d8fd349fbbb9bf53d6e5f49c2de448ad1de22
sirmcclay <mcclay.design@gmail.com>
2021-11-08 17:04:25 -0300

    row component and show index grid rows

--- 05-kickstart/pages/campaigns/requests/index.js
+++ 05-kickstart/pages/campaigns/requests/index.js
@@ -3,6 +3,7 @@ import { Button, Table } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
+import RequestRow from '../../../components/RequestRow';
 class RequestIndex extends Component {
 	static async getInitialProps(props) {
 		const { address } = props.query;
@@ -18,6 +19,18 @@ class RequestIndex extends Component {
 		);
 
 		return { address, requests, requestCount };
+	}
+
+	renderRows() {
+		return this.props.requests.map((request, index) => {
+			return (
+				<RequestRow
+					key={index}
+					request={request}
+					address={this.props.address}
+				/>
+			);
+		});
 	}
 
 	render() {
@@ -43,6 +56,7 @@ class RequestIndex extends Component {
 							<HeaderCell>Finalize</HeaderCell>
 						</Row>
 					</Header>
+					<Body>{this.renderRows()}</Body>
 				</Table>
 			</Layout>
 		);
